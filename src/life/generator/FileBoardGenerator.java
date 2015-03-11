0b9be90baad1a5f09936b404d87375c4ac687db2
Scott Finkel <sora.havok@gmail.com>
2015-03-11 00:12:56 -0700

    Added Board generation from files.
    Also added some resource files.

---
+++ src/life/generator/FileBoardGenerator.java
@@ -0,0 +1,35 @@
+package life.generator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileBoardGenerator implements BoardGenerator {
+
+	private static final String BASE_PATH = "res/";
+
+	private final String fileName;
+
+	public FileBoardGenerator(final String fileName) {
+		this.fileName = fileName;
+	}
+
+	@Override
+	public int[][] getBoard() {
+
+		List<int[]> intLists = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(BASE_PATH + fileName))) {
+			String str;
+			while ((str = br.readLine()) != null) {
+				intLists.add(Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return intLists.toArray(new int[0][0]);
+	}
+
+}
