8ce1362d8c587cb87bb296cf4c11a233d5db73a7
Scott Finkel <sora.havok@gmail.com>
2015-03-11 01:06:44 -0700

    Added Docs and new constructor to RandomBoardGenerator

--- src/life/generator/BoardGenerator.java
+++ src/life/generator/BoardGenerator.java
@@ -1,5 +1,10 @@
 package life.generator;
 
+@FunctionalInterface
 public interface BoardGenerator {
+	/**
+	 * Create the initial game board from a source.
+	 * @return the starting position for the game board
+	 */
 	int[][] getBoard();
 }
--- src/life/generator/RandomBoardGenerator.java
+++ src/life/generator/RandomBoardGenerator.java
@@ -7,20 +7,37 @@ public class RandomBoardGenerator implements BoardGenerator {
 	private final Random rand = new Random();
 	private final int height;
 	private final int width;
+	private final int maxStartingTiles;
 	
-	public RandomBoardGenerator(final int height, final int width) {
+	
+	/**
+	 * Board which starts in a random configuration.
+	 * @param height number of tiles which fit on the board from top to bottom
+	 * @param width number of tiles which fit on the board from side to side
+	 * @param maxStartingTiles randomly attempt to set this number of tiles to a living state
+	 */
+	public RandomBoardGenerator(final int height, final int width, int maxStartingTiles) {
 		this.height = height;
-		this.width = width;// * 2 + 1;
+		this.width = width;
+		this.maxStartingTiles = maxStartingTiles;
 	}
+	
+	/**
+	 * {@code maxStartingTiles} defaults to {@code height * width / 3}.
+	 * @see #RandomBoardGenerator(height,width,maxStartingTiles)
+	 */
+	public RandomBoardGenerator(final int height, final int width) {
+		this(height, width, height * width / 3);
+	}
+	
 	
 	@Override
 	public int[][] getBoard() {
 		final int[][] boardArray = new int[height][width];
 		
-		for(int i = 0; i < height*width/2; i++) {
+		for(int i = 0; i < maxStartingTiles; i++) {
 			boardArray[rand.nextInt(height)][rand.nextInt(width)] = 1;
 		}
 		return boardArray;
 	}
-
 }
