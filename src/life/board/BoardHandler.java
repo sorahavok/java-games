1fca945cf92a8abad3237c063d430bcbf0166673
Scott Finkel <sora.havok@gmail.com>
2015-03-11 00:19:57 -0700

    Created Hex board and board Handler

---
+++ src/life/board/BoardHandler.java
@@ -0,0 +1,46 @@
+package life.board;
+
+import life.ruleset.*;
+
+public class BoardHandler {
+
+	private final RuleSet rules;
+	
+	private int[][] boardArray;
+	private int[][] boardArrayBuffer;
+	
+	public BoardHandler(RuleSet rules, int[][] boardArray) {
+		this.rules = rules;
+		this.boardArray = boardArray;
+		this.boardArrayBuffer = new int[getHeight()][getWidth()];
+	}
+
+	public int getHeight() {
+		return boardArray.length;
+	}
+
+	public int getWidth() {
+		return boardArray[0].length;
+	}
+
+	
+	public int get(int y, int x) {
+		return boardArray[y][x];
+	}
+
+	
+	public void updateCell(int[] neighbors, int y, int x) {
+		BlockState nextState = rules.getState(neighbors, boardArray[y][x]);
+		switch (nextState) {
+			case STAY: boardArrayBuffer[y][x] = boardArray[y][x]; break;
+			case LIVE: boardArrayBuffer[y][x] = 1; break;
+			case DIE: boardArrayBuffer[y][x] = 0; break;
+			default: System.out.println("Error, Unknown BlockState: " + nextState);
+		}		
+	}
+
+	public void updateBoard() {
+		boardArray = boardArrayBuffer;
+		boardArrayBuffer = new int[getHeight()][getWidth()];
+	}
+}
---
+++ src/life/board/HexBoard.java
@@ -0,0 +1,66 @@
+package life.board;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
+public class HexBoard implements Board {
+
+	private final BoardHandler board;
+	private final int blockSize;
+	private final int height;
+	private final int width;
+	private final Image full;
+	private final Image empty;
+	private final ImageObserver imageObserver;
+	
+	
+	public HexBoard(BoardHandler board, int tileSize, Image full, Image empty, ImageObserver imageObserver) {
+		this.board = board;
+		this.blockSize = tileSize;
+		this.height = board.getHeight();
+		this.width = board.getWidth();
+		this.full = full;
+		this.empty = empty;
+		this.imageObserver = imageObserver;
+	}
+	
+	@Override
+	public void update() {
+		for(int y=0; y < height; y++) {
+			int aboveY = (y - 1 + height) % height;
+			int twoAboveY = (y - 2 + height) % height;
+			int belowY = (y + 1) % height;
+			int twoBelowY = (y + 2) % height;
+			
+			for(int x=0; x < width; x++) {
+				int leftX = (x - 1 + width) % width;
+
+				int[] adj = { 
+						board.get(twoAboveY, x),						
+						board.get(aboveY, leftX),
+						board.get(aboveY, x),
+						board.get(belowY, leftX),
+						board.get(belowY, x),
+						board.get(twoBelowY, x)};
+				board.updateCell(adj, y, x);
+			}
+		}
+		board.updateBoard();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		for(int y = 0; y < height; y++) {
+			
+			int yOffset = y % 2 == 0 ? 0 : blockSize/2;
+			int xOffset = y % 2 == 0 ? 0 : (int)(blockSize * .75); 
+			
+			for(int x = 0; x < width; x++) {
+				
+				Image img = board.get(y,x) == 0 ? empty : full;
+				g.drawImage(img, (int)(1.5*x*blockSize)+xOffset, y/2*blockSize+yOffset, blockSize, blockSize, imageObserver);
+			}
+		}
+	}
+
+}
