4f5170cc58e16ae085f693d7e324869f7c3b456d
Scott Finkel <sora.havok@gmail.com>
2015-03-11 00:22:21 -0700

    Made switching boards simple in main

--- src/life/Life.java
+++ src/life/Life.java
@@ -1,8 +1,10 @@
 package life;
 
 import java.awt.*;
+import java.io.*;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import life.board.*;
@@ -32,23 +34,47 @@ public class Life extends JPanel {
 		controller.render(g);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		JFrame frame = new JFrame("Life");
 
+//		Good rules for a HexBoard
 		Map<Integer, BlockState> rulesMap = ImmutableMap.of(
 				2, BlockState.STAY,
-				3, BlockState.LIVE
+				3, BlockState.LIVE,
+				5, BlockState.LIVE,
+				6, BlockState.LIVE
 				);
+
+//		Good rules for a Triangle4Board
+//		Map<Integer, BlockState> rulesMap = ImmutableMap.of(
+//				0, BlockState.STAY,
+//				2, BlockState.LIVE,
+//				3, BlockState.LIVE
+//				);
+		
+//		Good rules for a SquareBoard
+//		Map<Integer, BlockState> rulesMap = ImmutableMap.of(
+//				2, BlockState.STAY,
+//				3, BlockState.LIVE
+//				);
 		
 		RuleSet rules = new StandardRules(rulesMap);
 		
 		BoardGenerator gen = new RandomBoardGenerator(FRAME_HEIGHT/TILE_SIZE, FRAME_WIDTH/TILE_SIZE);
+//		BoardGenerator gen = new StaticBoardGenerator();
+//		BoardGenerator gen = new FileBoardGenerator("Pulsar");
 		
 		BoardHandler boardHandler = new BoardHandler(rules, gen.getBoard());
 		
-		Board board = new SquareBoard(boardHandler, TILE_SIZE);
+//		Board board = new SquareBoard(boardHandler, TILE_SIZE);
+//		Board board = new Triangle4Board(boardHandler, TILE_SIZE);
 		
-		Controller controller = new AlwaysPlay(frame, board, FPS);
+		Image full = ImageIO.read(new File("res/FilledHex.png"));
+		Image empty = ImageIO.read(new File("res/EmptyHex.png"));
+		Board board = new HexBoard(boardHandler, TILE_SIZE, full, empty, frame);
+		
+//		Controller controller = new AlwaysPlay(f, board, FPS);
+		Controller controller = new PlayPauseController(frame, board, FPS);
 
 		Life life = new Life(controller);
 		
